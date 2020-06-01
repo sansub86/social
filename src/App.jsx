e827f04806f1e875d2b057c0ee48cdd0c5503636
sansub86 <sun_mail_86@mail.ru>
2020-06-02 00:37:13 +1000

    95

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {compose} from "redux";
@@ -15,6 +13,10 @@ import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app_reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withSuspense";
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 class App extends React.Component {
     componentDidMount() {
@@ -30,8 +32,8 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/news' component={News}/>
                     <Route path='/settings' component={Settings}/>
@@ -50,9 +52,9 @@ const ContainerApp = compose(
     connect(mapStateToProps, {initializeApp})
 )(App);
 
-const SamuraiApp = () =>  {
+const SamuraiApp = () => {
     return <BrowserRouter>
-        <Provider  store={store}>
+        <Provider store={store}>
             <ContainerApp/>
         </Provider>
     </BrowserRouter>
